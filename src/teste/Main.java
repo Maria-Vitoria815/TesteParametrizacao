018a22cf12db8567a2b6c60d6cb15e8e6902fee0
Maria-Vitoria815 <111195864+Maria-Vitoria815@users.noreply.github.com>
2024-07-23 09:39:49 -0300

    ATU

--- src/teste/Main.java
+++ src/teste/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
       
         Scanner scanner = new Scanner(System.in);
-        Cadastros<Conta> cadastros = new Cadastros<Conta>();
+        Cadastros<Conta> cadastros = new Cadastros<>();
 
         while (true) {
 
